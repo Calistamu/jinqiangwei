cd63bc87b7827497b1b6f2c95dca2e14702f0131
mudou <41284875+Calistamu@users.noreply.github.com>
2019-10-13 00:14:25 +0800

    主页实现弹窗，国际版和国内版网页搞定

---
+++ jinqiangwei/js/scripts.js
@@ -0,0 +1,59 @@
+$(function () {
+
+    // init feather icons
+    feather.replace();
+
+    // init tooltip & popovers
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+    //page scroll
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        event.preventDefault();
+    });
+
+    //toggle scroll menu
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        //adjust menu background
+        if (scroll >= 100) {
+            $('.sticky-parallex').removeClass('navbar-dark').addClass('navbar-light').addClass('bg-white');
+            $('.btn-signup').removeClass('btn-secondary').addClass('btn-primary');
+            $('.navbar-toggler').removeClass('text-white').addClass('text-dark');
+        } else {
+            $('.sticky-parallex').removeClass('navbar-light').removeClass('bg-white').addClass('navbar-dark');
+            $('.btn-signup').removeClass('btn-primary').addClass('btn-secondary');
+            $('.navbar-toggler').removeClass('text-dark').addClass('text-white');
+        }
+
+        // adjust scroll to top
+        if (scroll >= 600) {
+            $('.scroll-top').addClass('active');
+        } else {
+            $('.scroll-top').removeClass('active');
+        }
+        return false;
+    });
+
+    // scroll top top
+    $('.scroll-top').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    /**Theme switcher - DEMO PURPOSE ONLY s*/
+    $('.switcher-trigger').click(function () {
+        $('.switcher-wrap').toggleClass('active');
+    });
+    $('.color-switcher ul li').click(function () {
+        var color = $(this).attr('data-color');
+        $('#theme-color').attr("href", "css/" + color + ".css");
+        $('.color-switcher ul li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
